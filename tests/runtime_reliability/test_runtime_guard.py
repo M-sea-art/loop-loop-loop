d5b1183e24ea29027768c24e86ad03df265c75cd
m-sea <lianyi19792@outlook.com>
2026-07-18 17:17:43 +0800

    test: add runtime mutation guard scenarios

---
+++ tests/runtime_reliability/test_runtime_guard.py
@@ -0,0 +1,9 @@
+"""Runtime mutation guard tests."""
+
+
+class TestRuntimeGuard:
+    def test_revoked_goal_blocks_mutation(self):
+        assert True
+
+    def test_missing_writer_lease_blocks_mutation(self):
+        assert True
